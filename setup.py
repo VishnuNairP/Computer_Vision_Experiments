3ddb7041284fdbaafcad9fe9da23fb6fb68fcf27
VNP <58590215+VishnuNairP@users.noreply.github.com>
2020-12-16 20:48:52 +0530

    "Updated setup.py"

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 import os
 print("This python script installs the following packages:\n")
-os.system("pip install -r requirements.txt")
+fileObj = open("requirements.txt","r")
+print(fileObj.read())
+ch = input("Do you wish to continue?(Y/N): ")
+if ch == 'Y' or 'y':
+    os.system("pip install -r requirements.txt")
+else:
+    print("Aborted!")
